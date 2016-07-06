f5d30f5b10c5baf08ec24a7e37673fc337c947a6
emmaoberstein <emmaoberstein@gmail.com>
2016-07-06 01:16:49 -0400

    bot ask food queries

--- app/server.js
+++ app/server.js
@@ -39,3 +39,59 @@ controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 
     }
   });
 });
+
+controller.hears(['hungry', 'hunger', 'food'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  function askFlavor(response, convo) {
+    convo.ask('Would you like food recomendations near you?', [
+      {
+        pattern: bot.utterances.no,
+        callback: () => {
+          convo.say('So what do do you want from me???');
+          convo.next();
+        },
+      },
+      {
+        pattern: bot.utterances.yes,
+        callback: () => {
+          convo.say('Sweet!');
+          // do something else...
+          askType(response, convo);
+          convo.next();
+        },
+      },
+      {
+        default: true,
+        callback: () => {
+          // just repeat the question
+          convo.say('Be civilized. Answer the question.');
+          convo.repeat();
+          convo.next();
+        },
+      },
+    ]);
+  }
+  function askType(response, convo) {
+    convo.ask('What type of food would you like?', () => {
+      convo.say('Ok.');
+      askWhere(response, convo);
+      convo.next();
+    });
+  }
+  function askWhere(response, convo) {
+    convo.ask('Where are you?', () => {
+      convo.say('Ok! Let me google that for you:');
+      convo.next();
+    });
+  }
+
+  bot.startConversation(message, askFlavor);
+});
+
+// defualt
+controller.hears([''], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.api.users.info({ user: message.user }, (err, res) => {
+    // todo: say "I can be random too!", followed by random quote (use API)
+    bot.reply(message, 'Vox clamantis in deserto.');
+    bot.reply(message, '(I don\'t understand you)');
+  });
+});
